47d03d35751b1735626e40fa8919a97fee038680
alejovp <alejovp@gmail.com>
2019-05-07 12:00:12 +0200

    starter project

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+export const App = () => (
+    <div>
+        <h1>X Buttons Library</h1>
+    </div>
+);
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const APP_DIR = path.resolve(__dirname, 'src');
+const BUILD_DIR = path.resolve(__dirname, 'public');
+const libraryName = 'x-buttons';
+const outputFile = `${libraryName}.js`;
+
+
+module.exports = {
+	entry: {
+		main: ['babel-polyfill', `${APP_DIR}/index.js`]
+	},
+	devServer: {
+		contentBase: BUILD_DIR,
+		publicPath: '/js/',
+		historyApiFallback: true,
+	},
+	output: {
+		filename: outputFile,
+		path: `${BUILD_DIR}/js`,
+		library: libraryName,
+		libraryTarget: 'umd',
+		umdNamedDefine: true
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(scss)$/,
+				use: [
+					'style-loader',
+					'css-loader',
+					'sass-loader'
+				],
+			},
+			{
+				test: /\.jsx?$/,
+				include: [
+					APP_DIR,
+				],
+				loader: 'babel-loader'
+			},
+			{
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                include : APP_DIR,
+                options: {
+                    name: '[name]-[sha256:hash:base64:4].[ext]',
+                    publicPath: 'images/loader/',
+                    outputPath: '../images/loader/'
+                }
+            },
+		]
+	},
+};
