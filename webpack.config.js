120dab06a81e099ba61377352562b629f6d25036
avazquez <avazquez@leadtech.com>
2019-05-08 13:44:23 +0200

    New lib files in bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const APP_DIR = path.resolve(__dirname, 'src');
+const LIB_DIR = path.resolve(__dirname, 'src/components');
 const BUILD_DIR = path.resolve(__dirname, 'public');
 const libraryName = 'x-buttons-lib';
 const outputFile = `${libraryName}.js`;
@@ -7,7 +7,7 @@ const outputFile = `${libraryName}.js`;
 
 module.exports = {
 	entry: {
-		main: `${APP_DIR}/index.js`
+		main: `${LIB_DIR}/index.js`
 	},
 	devServer: {
 		contentBase: BUILD_DIR,
@@ -33,14 +33,14 @@ module.exports = {
 			{
 				test: /\.jsx?$/,
 				include: [
-					APP_DIR,
+					LIB_DIR,
 				],
 				loader: 'babel-loader'
 			},
 			{
                 test: /\.(png|jpg|gif|svg)$/,
                 loader: 'base64-image-loader',
-                include : APP_DIR,
+                include : LIB_DIR,
                 options: {
                     name: '[name]-[sha256:hash:base64:4].[ext]',
                     publicPath: 'images/loader/',
