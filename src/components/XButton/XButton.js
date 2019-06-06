52a105d01a15daf4849d889c27c025f5c20c2ef4
alejovp <alejovp@gmail.com>
2019-06-06 09:40:08 +0200

    XButton NOK version created

--- src/components/XButton/XButton.js
+++ src/components/XButton/XButton.js
@@ -1,14 +1,19 @@
 import React from 'react';
-
 import wImg from './img/w1.png';
+import wImgNo from './img/w2.png';
+
+import './XButton.scss';
 
 
 export const XButton = props => {
-    const { children } = props;
+    const { children, variant } = props;
+
     return (
-        <button>
-            <img src={wImg}/>
-            {children}
+        <button className="x-btn">
+            <img className="x-img"
+                 src={variant === 'nok' ? wImgNo : wImg}
+            />
+                {children}
         </button>
     );
 };
