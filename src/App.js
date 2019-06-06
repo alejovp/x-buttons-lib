63e628aafce199d1720f5c9bf77101956d771c0f
alejovp <alejovp@gmail.com>
2019-06-06 09:40:26 +0200

    XButton NOK version added to lib app

--- src/App.js
+++ src/App.js
@@ -5,6 +5,21 @@ import { XButton } from './components/XButton/XButton';
 export const App = () => (
     <div>
         <h1>X Buttons Library</h1>
-        <XButton></XButton>
+        <hr/>
+        <h3>OK Button:</h3>
+        <div>
+            <XButton></XButton>
+            <div className="code-cont">
+                <span className="code-txt">{'<XButton></XButton>'}</span>
+            </div>
+        </div>
+        <hr/>
+        <h3>NOK Button:</h3>
+        <div>
+            <XButton variant="nok"></XButton>
+            <div className="code-cont">
+                <span className="code-txt">{'<XButton variant="nok"></XButton>'}</span>
+            </div>
+        </div>
     </div>
 );
