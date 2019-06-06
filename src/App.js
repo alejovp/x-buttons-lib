73a6478ebcc32328fada04b0391ef1694863a2bf
alejovp <alejovp@gmail.com>
2019-06-06 11:10:30 +0200

    Lib and Button styles update

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ export const App = () => (
     <div>
         <h1>X Buttons Library</h1>
         <hr/>
+        <hr/>
         <h3>OK Button:</h3>
-        <div>
+        <div className="comp-cont">
             <XButton></XButton>
             <div className="code-cont">
                 <span className="code-txt">{'<XButton></XButton>'}</span>
@@ -15,11 +16,12 @@ export const App = () => (
         </div>
         <hr/>
         <h3>NOK Button:</h3>
-        <div>
+        <div className="comp-cont">
             <XButton variant="nok"></XButton>
             <div className="code-cont">
                 <span className="code-txt">{'<XButton variant="nok"></XButton>'}</span>
             </div>
         </div>
+        <hr/>
     </div>
 );
